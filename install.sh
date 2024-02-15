e483aefd6301eab51161db27f38a59e13fa6e302
K4R7IK <kartik.workspace@gmail.com>
2024-02-15 11:46:05 +0530

    Minor fix up.
    Check for .local/bin path during installation. (WSL)
    Modified the installation scripts with minor best practices changes.

--- install.sh
+++ install.sh
@@ -1,8 +1,16 @@
-# Script that install devexcuse script in /usr/local/bin
+#!/bin/bash
+
+# Set destination directory
+DEST=$HOME/.local/bin
+
+# Check if $DEST exists and create if not
+if [ ! -d "$DEST" ]; then
+  mkdir -p "$DEST"
+fi
 
 # Getting the package manager
 get_package_manager() {
-  if command -v apt-get &>/dev/null; then
+  if command -v apt &>/dev/null; then
     PACKAGE_MANAGER="apt"
   elif command -v yum &>/dev/null; then
     PACKAGE_MANAGER="yum"
@@ -21,17 +29,17 @@ get_package_manager() {
 install_dependencies() {
   case $PACKAGE_MANAGER in
     "apt")
-      sudo apt-get update
-      sudo apt-get install -y ${MISSING_DEPENDENCIES[@]}
+      sudo apt update
+      sudo apt install -y "${MISSING_DEPENDENCIES[@]}"
       ;;
     "yum" | "dnf")
-      sudo yum install -y ${MISSING_DEPENDENCIES[@]}
+      sudo yum install -y "${MISSING_DEPENDENCIES[@]}"
       ;;
     "pacman")
-      sudo pacman -Sy --noconfirm ${MISSING_DEPENDENCIES[@]}
+      sudo pacman -Sy --noconfirm "${MISSING_DEPENDENCIES[@]}"
       ;;
     "zypper")
-      sudo zypper install -y ${MISSING_DEPENDENCIES[@]}
+      sudo zypper install -y "${MISSING_DEPENDENCIES[@]}"
       ;;
     *)
       echo "Unknown package manager. Please install curl and jq manually."
@@ -39,11 +47,12 @@ install_dependencies() {
   esac
 }
 
+# Check dependencies
 check_dependencies() {
   get_package_manager
   MISSING_DEPENDENCIES=()
 
-  # Check for curl
+# Check for curl
   if ! command -v curl &>/dev/null; then
     MISSING_DEPENDENCIES+=("curl")
   fi
@@ -62,12 +71,23 @@ check_dependencies() {
   fi
 }
 
+# Call function to check dependencies
 check_dependencies
+
+# Create .config directory if not exist
+CONFIG_DIR="$HOME/.config"
+if [ ! -d "$CONFIG_DIR" ]; then
+  mkdir -p "$CONFIG_DIR"
+fi
+
+# Download excuse database
 echo "Installing Excuse database..."
-curl -fsSL https://raw.githubusercontent.com/K4R7IK/DevExcuse/master/excuse.json -o $HOME/.config/excuse.json
+curl -fsSL https://raw.githubusercontent.com/K4R7IK/DevExcuse/master/excuse.json -o "$CONFIG_DIR/excuse.json"
 
+# Download devexcuse script
 echo "Installing devexcuse..."
-curl -fsSL https://raw.githubusercontent.com/K4R7IK/DevExcuse/master/devexcuse.sh -o $HOME/.local/bin/devexcuse
+curl -fsSL https://raw.githubusercontent.com/K4R7IK/DevExcuse/master/devexcuse.sh -o "$DEST/devexcuse"
 
+# Set executable permissions
 echo "Setting executable permissions..."
-chmod +x $HOME/.local/bin/devexcuse
+chmod +x "$DEST/devexcuse"
